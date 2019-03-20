b2cf1fb8fa9b9098b625889c3ef7b9036c4f2cfc
yhoupert <yhoupert@linagora.com>
2019-03-20 11:44:25 +0100

    add license

--- pollution/api/pollutionApi.js
+++ pollution/api/pollutionApi.js
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2017 Linagora.
+ * Copyright (c) 2018 Linagora.
  *
- * This file is part of Business-Logic-Server
+ * This file is part of Linto-Skills-Optional
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 const debug = require('debug')('redmanager:flow:optional:skill:pollution:api')
 const PollutionPrevair = require('./prevair')
 
